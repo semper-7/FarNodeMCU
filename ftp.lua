25219b7a39ab6767b9bf5cc8145c4ddc6ca7bd89
semper-7 <semper-7@yandex.ru>
2020-11-10 22:48:48 +0700

    Edited ftp.lua

--- ftp.lua
+++ ftp.lua
@@ -2,6 +2,7 @@
 s=net.createServer(net.TCP, 30)
 s:listen(21,function(c)
  c:on("receive",function(c,l)
+  uart.write(0,l)
   if l:sub(1,4)=="QUIT" then
    c:send("221 Goodbye\n")
    I=false
@@ -26,21 +27,25 @@ s:listen(21,function(c)
    if I then
     if l:sub(1,5)=="DELE " then
      file.remove(l:match("%S+",6))
-     c:send("250 OK")
+     c:send("250 OK\n")
     elseif l:sub(1,3)=="PWD" then
-     c:send('257 "/" - is only directory\n')
+     c:send('257 "/" - only folder\n')
+    elseif l:sub(1,3)=="CWD" then
+     c:send("250 OK\n")
+    elseif l:sub(1,4)=="SYST" then
+     c:send("215 UNIX Type: L8\n")
+    elseif l:sub(1,4)=="FEAT" then
+     c:send("211-Features:\nPASV\nSIZE\n211 End\n")
     elseif l:sub(1,4)=="PASV" then
      c:send("227 Entering Passive Mode \("..string.gsub(wifi.sta.getip(),"%p+",",")..",21,179\)\n")
      p=net.createServer(net.TCP, 30)
-      p:listen(5555,function(c)
-      c:send("FTP data server\n")
-      d=c
-      end)
+     p:listen(5555,function(c) d=c end)
+    elseif l:sub(1,4)=="TYPE" then
+     c:send("200 OK\n")
     elseif l:sub(1,4)=="LIST" then
      if d then
-      c:send("150 Filelist started\n")
-      d:send("Filename\tSize\n")
-      for f,s in pairs(file.list()) do d:send(f.."   \t"..s.."\n") end
+      c:send("150 File listing started\n")
+      for f,s in pairs(file.list()) do d:send("-rw-r--r-- 1 0 0 "..s.."	01 Jan 20 00:00 "..f.."\n") end
       c:send("226 Filelist sended\n")
      else
       c:send("550 Enter PASV and connect first\n")
